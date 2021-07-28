d715f088e1c71a0a03b0e53137a6b5ef323e3bf8
dinhhuychuong3005 <dinhhuychuong3005@gmail.com>
2021-07-28 15:35:52 +0700

    [Bài tập] Tích hợp RESTful cho ứng dụng Blog

---
+++ src/main/java/com/codegym/service/impl/blog/IBlogService.java
@@ -0,0 +1,13 @@
+package com.codegym.service.impl.blog;
+
+import com.codegym.model.Blog;
+import com.codegym.model.Category;
+import com.codegym.service.IGeneralService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IBlogService extends IGeneralService<Blog> {
+    Iterable<Blog> findAllByCategory(Category category);
+
+    Page<Blog> findAllByTitleContaining(String title,Pageable pageable);
+}
